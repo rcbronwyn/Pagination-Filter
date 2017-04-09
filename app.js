e9645659bafd28e395e311dcf29a8e557ee48ea7
rcbronwyn <rcbronwyn@gmail.com>
2017-04-08 20:21:09 -0400

    Working Pagination Update
    Pagination now accurately reflects the number of student records, and
    the click event now works correctly.  Started working on search
    functionality.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 ﻿const studentList = document.getElementsByClassName('student-item');
 const page = document.getElementsByClassName('page')[0];
+const header = document.getElementsByClassName('page-header')[0];
 
 //Hide students 11 and up.
 for (let i = 0; i < studentList.length; i += 1) {
@@ -19,10 +20,10 @@ let pageList = document.createElement('ul');
 page.appendChild(pagination);
 pagination.appendChild(pageList);
 
-for (let i = 1; i < studentList.length / 10; i += 1) {
+for (let i = 0; i < studentList.length / 10; i += 1) {
     const button = document.createElement('li');
     const buttonLink = document.createElement('a');
-    buttonLink.textContent = i;
+    buttonLink.textContent = i + 1;
     buttonLink.href = "#";
 
     button.appendChild(buttonLink);
@@ -30,17 +31,37 @@ for (let i = 1; i < studentList.length / 10; i += 1) {
 }
 
 
+//Create the search input group.
+let search = document.createElement('div');
+search.className = "student-search";
+let searchBar = document.createElement('input');
+searchBar.placeholder = "Search for student records...";
+let searchBttn = document.createElement('button');
+searchBttn.textContent = "Search";
+header.appendChild(search);
+search.appendChild(searchBar);
+search.appendChild(searchBttn);
+
+
 
 //Lets add event listeners to the pagination!
 let pageLinks = document.querySelectorAll('.pagination ul li a');
 let pageCount = pageLinks.length;
-console.log(pageCount);
 
-
+    
 for (let i = 0; i < pageCount; i++) {
+    let lowEnd = i * 10;
+    let highEnd = i * 10 + 10;
+
+
+
     pageLinks[i].addEventListener('click', () => {
-        for (let k = 0; k < i * 10; k++) {
-            studentList[k].style.display = "block";
-        };
+        for (let k = 0; k < studentList.length; k += 1) {
+            if (k < highEnd && k >= lowEnd) {
+                studentList[k].style.display = 'block';
+            } else {
+                studentList[k].style.display = 'none';
+            }
+        }
     });
 }
